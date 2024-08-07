124feabf0f430d1e338f57f7040a6f1d2d42592a
Olumide Ogunseye <olumideogunseye@Olumides-MacBook-Pro-514.local>
2024-08-07 15:37:25 +0100

    html template edit with bootstrap, error showing on front-end

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, jsonify, render_template, url_for, request, redirect
-import json
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 from datamanager.data_json import JSONDataManager
-import uuid
 
 app = Flask(__name__)
-data_manager = JSONDataManager('data/data.json')
+
+try:
+    # Code that might raise an exception
+    data_manager = JSONDataManager('data/data.json')
+except IOError as e:
+    # Code to handle the exception
+    print("An IOError occurred: ", str(e))
 
 
 @app.route('/')
 def home():
-    return "Welcome to MovieWeb App!"
+    return render_template('index.html')
 
 
 @app.route('/users')
@@ -19,12 +23,19 @@ def list_users():
     return render_template('users.html', users=users)
 
 
+@app.route('/ManageUsers')
+def manage_users():
+    data = data_manager.get_all_users()
+    users = [{"id": user_id, "name": user_info["name"]} for user_id, user_info in data.items()]
+    return render_template('manage_users.html', users=users)
+
+
 @app.route('/users/<user_id>', methods=['GET'])
 def user_movies(user_id):
     user = data_manager.get_user_name(user_id)
     movies = data_manager.get_user_movies(user_id)
 
-    return render_template('movies.html', movies=movies, user=user, user_id=user_id)  # Temporarily returning users as a string
+    return render_template('movies.html', movies=movies, user=user, user_id=user_id)
 
 
 @app.route('/add_user', methods=['GET', 'POST'])
@@ -34,24 +45,37 @@ def add_user():
         all_users = data_manager.get_all_users_names()
         while new_user in all_users:
             error = 'User already exists, kindly add a unique name'
-            return render_template('add_user.html', message=error)
+            return render_template('add_user.html', error=error)
         else:
-            data_manager.add_user(new_user)
-            return redirect(url_for('list_users'))
+            response = data_manager.add_user(new_user)
+            if 'message' in response:
+                message = response['message']
+                return render_template('add_user.html', message=message)
     return render_template('add_user.html')
+
+
+@app.route('/users/delete_user/<user_id>', methods=['POST'])
+def delete_user(user_id):
+    data_manager.del_user(user_id)
+    return redirect(url_for('manage_users'))
+
+
+@app.route('/users/update_user/<user_id>', methods=['GET', 'POST'])
+def update_user(user_id):
+    if request.method == 'POST':
+        data_manager.update_user(user_id)
+        return redirect(url_for('manage_users'))
+
+    users = data_manager.get_all_users()
+    user = users.get(str(user_id))
+    return render_template('update_user.html', user=user, user_id=user_id)
 
 
 @app.route('/users/<user_id>/add_movie', methods=['GET', 'POST'])
 def add_movie(user_id):
     if request.method == 'POST':
-        movie = {
-            'id': str(uuid.uuid4()),  # Convert UUID to string for JSON serialization
-            'title': request.form['title'],
-            'director': request.form['director'],
-            'year': request.form['year'],
-            'rating': request.form['rating']
-        }
-        data_manager.add_user_movie(user_id, movie)
+        movie_title = request.form['title']
+        data_manager.add_user_movie(user_id, movie_title)
         return redirect(url_for('user_movies', user_id=user_id))
 
     return render_template('add_movie.html', user_id=user_id)
@@ -59,9 +83,27 @@ def add_movie(user_id):
 
 @app.route('/users/<user_id>/update_movie/<movie_id>', methods=['GET', 'POST'])
 def update_movie(user_id, movie_id):
+    old_movie = None
+    error = None
 
     if request.method == 'POST':
-        data_manager.update_user_movie(user_id, movie_id)
+
+        response = data_manager.update_user_movie(user_id, movie_id)
+        if "error" in response:
+            error = response['error']
+
+            movie = {  # This movie retains current data in form while we update info
+                'id': movie_id,
+                'title': request.form['title'],
+                'year': request.form['year'],
+                'director': request.form['director'],
+                'rating': request.form['rating']
+            }
+
+            return render_template('update.html', user_id=user_id, error=error, movie_id=movie_id, old_movie=movie)
+        elif "message" in response:
+            message = response['message']
+            return render_template('update.html', user_id=user_id, message=message, old_movie=old_movie)
         return redirect(url_for('user_movies', user_id=user_id, movie_id=movie_id))
 
     users = data_manager.get_all_users()
@@ -70,15 +112,13 @@ def update_movie(user_id, movie_id):
         for movie in user['movies']:
             if str(movie['id']) == str(movie_id):
                 old_movie = movie
-                return render_template('update.html', user_id=user_id, old_movie=old_movie)
+                break
+    return render_template('update.html', user_id=user_id, old_movie=old_movie, error=error)
 
 
-@app.route('/users/<user_id>/delete_movie/<movie_id>', methods=['GET', 'POST'])
+@app.route('/users/<user_id>/delete_movie/<movie_id>', methods=['POST'])
 def delete_movie(user_id, movie_id):
-    if request.method == 'POST':
-        data_manager.delete_user_movie(user_id, movie_id)
-        return redirect(url_for('user_movies', user_id=user_id))
-
+    data_manager.delete_user_movie(user_id, movie_id)
     return redirect(url_for('user_movies', user_id=user_id))
 
 
@@ -88,23 +128,38 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(FileNotFoundError)
+def page_not_found(error):
+    return render_template('file_not_found.html'), 404
+
+
+@app.errorhandler(KeyError)
+def page_not_found(error):
+    return render_template('add_movie.html'), 404
+
+
 # Route for handling other errors (e.g., 500)
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html'), 500
 
 
+@app.errorhandler(TypeError)
+def type_error(error):
+    return render_template('400.html'), 400
+
+
 # Route for handling other errors (e.g., 400)
-@app.errorhandler(400)
-def internal_server_error(error):
+@app.errorhandler(ValueError)
+def handle_value_error(error):
     return render_template('400.html'), 400
 
 
 # Custom error handler for FileNotFoundError
-@app.errorhandler(FileNotFoundError)
+@app.errorhandler(405)
 def handle_file_not_found_error(error):
     return render_template('405.html'), 405
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5003, host='0.0.0.0', debug=True)
--- datamanager/data_json.py
+++ datamanager/data_json.py
@@ -1,9 +1,43 @@
 import json
-import os
 from flask import request
 from .data_manager import DataManagerInterface
+import requests
+import uuid
+import datetime
 
-file_path = '../data/data.json'
+API_KEY = "aeb2943f"
+REQUEST_URL = f"https://www.omdbapi.com/?apikey={API_KEY}&t="
+
+
+def get_movie_from_api(movie_title):
+    """
+    Helper function to get movie data from IMDP Api
+    :param movie_title:
+    :return: dict{movie} data. Title, Year, Poster, Rating, Director,
+    """
+    url = REQUEST_URL + movie_title
+    try:
+        response = requests.get(url)
+        if response.status_code == requests.codes.ok:
+            imdb_movie = response.json()
+
+            default_poster = "/static/uploads/images/default_image.png"
+
+            movie = {
+                'id': str(uuid.uuid4()),  # Convert UUID to string for JSON serialization
+                'title': imdb_movie["Title"],
+                'year': imdb_movie["Year"][:4],
+                'poster': default_poster if imdb_movie["Poster"] == 'N/A' else imdb_movie["Poster"],
+                'rating': imdb_movie["imdbRating"],
+                'director': imdb_movie["Director"]
+            }
+            return movie
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+    except ValueError as e:
+        print(f"JSON decoding failed: {e}")
+        return None
 
 
 class JSONDataManager(DataManagerInterface):
@@ -40,14 +74,34 @@ class JSONDataManager(DataManagerInterface):
         self._save_all_users(users)
         return {'message': f"New user {users[new_id]['name']} Added successfully"}
 
+    def del_user(self, user_id):
+        users = self.get_all_users()
+        if str(user_id) in users:
+            del users[str(user_id)]
+            self._save_all_users(users)
+            return {'message': f"User ID {user_id} deleted successfully"}
+        raise ValueError(f"User with ID {user_id} not found")
+
+    def update_user(self, user_id):
+        users = self.get_all_users()
+        if str(user_id) in users:
+            updated_name = request.form['name']
+            user = users.get(str(user_id))
+            if updated_name in users:
+                return {'message': f"User {user['name']} already exits"}
+            user['name'] = updated_name
+            self._save_all_users(users)
+            return {'message': f"User ID {user_id} updated successfully"}
+        raise ValueError(f"User with ID {user_id} not found")
+
     def get_user_movies(self, user_id):
         users = self.get_all_users()
         user = users.get(str(user_id))
         if user:
             return user['movies']
-        return []
+        return TypeError(f"User with ID {user_id} not found")
 
-    def add_user_movie(self, user_id, movie):
+    def add_user_movie(self, user_id, movie_title):
         """Input: given a user_id, Method takes in a dictionary containing data of a new movie,
         and appends to the user_movie list.
             {
@@ -58,16 +112,22 @@ class JSONDataManager(DataManagerInterface):
          """
         users = self.get_all_users()
         user = users.get(str(user_id))
-        if user:
-            for existing_movie in user['movies']:
-                if existing_movie['title'] == movie['title']:
-                    return {'message': f"The movie title {movie['title']} already exists"}
-            user['movies'].append(movie)
-        else:
-            return {'message': f"User with ID {user_id} not found"}
+        try:
+            if user:
+                for existing_movie in user['movies']:
+                    if existing_movie['title'].lower() == movie_title.lower():
+                        return {'message': f"The movie title {movie_title} already exists"}
 
-        self._save_all_users(users)
-        return {'message': f"The movie title {movie['title']} Added successfully"}
+                new_movie = get_movie_from_api(movie_title)
+                if new_movie is not None:
+                    user['movies'].append(new_movie)
+                    self._save_all_users(users)
+                    return {'message': f"The movie title {movie_title} Added successfully"}
+                return {'message': f"Internet Error"}
+            else:
+                return {'message': f"User with ID {user_id} not found"}
+        except KeyError:
+            return {'message': f"Movie may not exists. try rephrasing the title"}
 
     def update_user_movie(self, user_id, movie_id):
         """
@@ -85,12 +145,35 @@ class JSONDataManager(DataManagerInterface):
                     # Update movie details based on the new input received
                     movie['title'] = request.form['title']
                     movie['director'] = request.form['director']
-                    movie['year'] = request.form['year']
-                    movie['rating'] = request.form['rating']
+
+                    # Validate and update year
+                    while True:
+                        try:
+                            year = int(request.form['year'])
+                            current_year = datetime.datetime.now().year
+                            if 1900 <= year <= current_year:  # Example range check
+                                movie['year'] = year
+                                break
+                            else:
+                                return {"error": "Year is out of range. Please enter a valid year."}
+                        except ValueError:
+                            return {"error": "Invalid year input. Please enter a valid year."}
+
+                    # Validate and update rating
+                    while True:
+                        try:
+                            rating = float(request.form['rating'])
+                            if 1.0 <= rating <= 10.0:
+                                movie['rating'] = round(rating, 1)
+                                break
+                            else:
+                                return {"error": "Rating must be between 1 and 10. Please enter a valid rating."}
+                        except ValueError:
+                            return {"error": "Invalid rating input. Please enter a valid rating."}
 
                     self._save_all_users(users)
-                    return {'message': f"Movie ID {movie_id} updated successfully"}
-        raise ValueError("Movie not found")
+                    return {'message': f"Movie updated successfully"}
+        return {"error": "Movie not found"}
 
     def delete_user_movie(self, user_id, movie_id):
         users = self.get_all_users()
@@ -106,18 +189,3 @@ class JSONDataManager(DataManagerInterface):
     def _save_all_users(self, users):
         with open(self.filename, 'w') as file:
             json.dump(users, file, indent=4)
-
-# bob = JSONDataManager(file_path)
-# # print(bob.get_all_users())
-# # print(bob.get_user_movies(2))
-# movie = {
-#         "id": 90,
-#         "title": "Dunes",
-#         "director": "Nolan",
-#         "year": 2017,
-#         "rating": 9.8
-#       }
-# # #
-# print(bob.add_user_movie(1, movie))
-# #
-# # print(bob.get_all_users_names())
